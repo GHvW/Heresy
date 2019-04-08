8053929cade93720f9ad800911bff8323b7d53a1
Garrett van Wageningen <ghvanw283@gmail.com>
2019-04-07 22:46:01 -0500

    initial commit

---
+++ NullableExtensions/NullableReferenceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Heresy {
+  public static class NullableReferenceExtensions {
+
+    public static U? Map<T, U>(this T? x, Func<T, U> fn)
+      where T : class
+      where U : class {
+
+      return x switch {
+        null => null,
+        T it => fn(it)
+      };
+
+    }
+
+    public static U? FlatMap<T, U>(this T? x, Func<T, U?> fn)
+      where T : class
+      where U : class {
+
+      return x switch {
+        null => null,
+        T it => fn(it)
+      };
+    }
+  }
+}
